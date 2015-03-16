686d27f944297db4b426cff5f74cd6008fc5e091
Alex Harvey <Alex.Harvey@team.telstra.com>
2015-03-16 12:11:42 +1100

    Issue_9 Update tests for pulp-manage-db

--- spec/classes/shared_server_setup_db.rb
+++ spec/classes/shared_server_setup_db.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 shared_context :server_setup_db do
   it { should contain_exec('setup-pulp-db').with({
     :command => '/usr/bin/pulp-manage-db',
+    :user => 'apache',
     :refreshonly => 'true',
     :tries => 12,
     :try_sleep => 10,
